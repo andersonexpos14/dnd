e0b3288a7cfdbeb0e00babc9980e4ddac6752522
Owen Newberry <owen@owennewberry.com>
2024-09-11 15:16:15 -0400

    added index and some party stuff

---
+++ party/manageParty.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo 'You have reached the manage party page.';
+    ?>
+  </body>
+</html>
